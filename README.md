b434e3b5dc6b4b0c8d4343b7e56739a5aacd424c
Anton Tolchanov <commits@knyar.net>
2022-04-06 20:03:09 +0200

    update install script and instructions

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Export whatsapp timestamps
 
-Install Python from https://knyar.net/temp/python-3.10.4-macos11.pkg
-Run `run.sh`
+- Install Python from https://knyar.net/temp/python-3.10.4-macos11.pkg
+- Grant Terminal full-disk access in Security preferences
+- Run `run.sh`
--- run.sh
+++ run.sh
@@ -2,11 +2,13 @@
 
 set -e -u -o pipefail
 
-pip3.10 install git+https://github.com/KnugiHK/iphone_backup_decrypt
+pip3.10 install https://github.com/KnugiHK/iphone_backup_decrypt/archive/refs/heads/master.zip
 pip3.10 install whatsapp-chat-exporter
+pip3.10 install requests
 
 id=$(ls "$HOME/Library/Application Support/MobileSync/Backup/" | head -1)
 
-wtsexporter -i -b "$HOME/Library/Application Support/MobileSync/Backup/$id"
+/Library/Frameworks/Python.framework/Versions/3.10/bin/wtsexporter \
+  -i -b "$HOME/Library/Application Support/MobileSync/Backup/$id"
 
 python3.10 timestamps.py > output.csv
